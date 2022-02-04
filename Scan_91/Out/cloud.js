c47baf9e9242de24e2ec6de51b2b4a1602803ee9
Jonathan <jgimmink@gmail.com>
2022-02-04 14:06:00 +0100

    Added Scan_91

---
+++ Scan_91/Out/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2291443,
+    "boundingBox": {
+        "lx": 111776.36759281159,
+        "ly": 397827.84453201296,
+        "lz": 1.3836756740381873e-10,
+        "ux": 111848.635222435,
+        "uy": 397900.11216163638,
+        "uz": 72.26762962355146
+    },
+    "tightBoundingBox": {
+        "lx": 111776.36759281159,
+        "ly": 397827.84453201296,
+        "lz": 1.3836756740381873e-10,
+        "ux": 111796.39159281159,
+        "uy": 397900.1125320129,
+        "uz": 4.371000000138368
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.0,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
