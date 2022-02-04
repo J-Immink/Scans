f9489b14e5dd5971a6b7ef9f43936c10c3625fbb
Jonathan <jgimmink@gmail.com>
2022-02-04 14:17:08 +0100

    Added scan_70

---
+++ Scan_70/Out/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1659405,
+    "boundingBox": {
+        "lx": 111806.31750106812,
+        "ly": 397871.1156978607,
+        "lz": -0.18136140704154969,
+        "ux": 111888.50454711914,
+        "uy": 397953.30274391177,
+        "uz": 82.00568464398384
+    },
+    "tightBoundingBox": {
+        "lx": 111806.31750106812,
+        "ly": 397871.1156978607,
+        "lz": -0.18136140704154969,
+        "ux": 111888.50450106812,
+        "uy": 397894.4806978607,
+        "uz": 2.6036385929584506
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.0,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
